b89c8c8984923b825e35a7004ed102223c88ce05
jamdewalt <98490986+jamdewalt@users.noreply.github.com>
2022-03-23 14:29:15 -0500

    Add files via upload

---
+++ unit-3/js/main.js
@@ -0,0 +1,20 @@
+//execute script when window is loaded
+window.onload = function(){
+
+    var container = d3.select("body") //get the <body> element from the DOM
+
+};
+
+//Example 1.1 line 3...container block
+var container = d3.select("body") //get the <body> element from the DOM
+    .append("svg") //put a new svg in the body
+
+    //SVG dimension variables
+    var w = 900, h = 500;
+
+     //Example 1.2 line 1...container block
+    var container = d3.select("body") //get the <body> element from the DOM
+        .append("svg") //put a new svg in the body
+        .attr("width", w) //assign the width
+        .attr("height", h) //assign the height
+        .attr("class", "container") //always assign a class (as the block name) for styling and future selection
